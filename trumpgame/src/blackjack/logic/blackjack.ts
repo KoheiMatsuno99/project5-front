b47f8cb9621da5ce5616c6703becd73875f66838
kohei <koheimtri3@gmail.com>
2023-10-14 21:09:41 +0900

    勝敗判定

--- trumpgame/src/blackjack/logic/blackjack.ts
+++ trumpgame/src/blackjack/logic/blackjack.ts
@@ -4,30 +4,12 @@ class Player{
     private money: number = 100
     private hand: Card[] = []
     private status: string = "playing" // "playing" or "bust" or "stand"
+    private result?: string // "win" or "lose" or "draw" ディーラーの場合はundefined
+    private bet: number = 0
 
     constructor(name: string, type: string){
         this.name = name
         this.type = type
-    }
-
-    public getName(): string{
-        return this.name
-    }
-
-    public getType(): string{
-        return this.type
-    }
-
-    public getMoney(): number{
-        return this.money
-    }
-
-    public getHand(): Card[]{
-        return this.hand
-    }
-
-    public getStatus(): string{
-        return this.status
     }
 
     public calcScore(): number{
@@ -51,8 +33,49 @@ class Player{
         return score
     }
 
+    public getName(): string{
+        return this.name
+    }
+
+    public getType(): string{
+        return this.type
+    }
+
+    public getMoney(): number{
+        return this.money
+    }
+
+    public getHand(): Card[]{
+        return this.hand
+    }
+
+    public getStatus(): string{
+        return this.status
+    }
+
+    public getResult(): string | undefined{
+        return this.result
+    }
+
+    public getBet(): number{
+        return this.bet
+    }
+
     public setStatus(status: string): void{
         this.status = status
+    }
+
+    public setResult(result: string): void{
+        this.result = result
+    }
+
+    public setBet(bet: number): void{
+        this.bet = bet
+        this.money -= bet
+    }
+
+    public setMoney(money: number): void{
+        this.money = money  
     }
 }
 
@@ -75,9 +98,10 @@ class Card{
 }
 
 class Table{
-    private players: Player[]
+    private players: Player[] // players[0]はディーラー
     private deck: Card[]
     private winner?: Player
+    private round: number = 0
 
     constructor(players: Player[]){
         this.players = players
@@ -99,7 +123,9 @@ class Table{
     }
 
     private startRound(): void{
+        this.round++
         for (const player of this.players){
+            player.setStatus("playing")
             for(let i = 0; i < 2; i++){
                 this.dealCard(player)
             }
@@ -118,6 +144,45 @@ class Table{
 
     private dealCard(player: Player): void{
         player.getHand().push(this.deck.pop()!)
+    }
+
+    public judgeResult(): void{
+        const dealerScore = this.players[0].calcScore()
+        for(let i = 1; i < this.players.length; i++){
+            const playerScore = this.players[i].calcScore()
+            if(playerScore >= 22){
+                this.players[i].setResult("lose")
+            }else if (playerScore === dealerScore){
+                this.players[i].setResult("draw")
+            }else{
+                playerScore > dealerScore ? this.players[i].setResult("win") : this.players[i].setResult("lose")
+            }
+        }
+    }
+
+    //賭け金を払い戻す
+    public payBack(): void{
+        for(let i = 1; i < this.players.length; i++){
+            const player = this.players[i]
+            const result = player.getResult()
+            const bet = player.getBet()
+            if(result === "win"){
+                player.setMoney(player.getMoney() + bet * 2)
+            }else if(result === "draw"){
+                player.setMoney(player.getMoney() + bet)
+            }
+        }
+    }
+    
+    public judgeFinalResult(): void{
+        let maxAmountOfMoney = 0
+        for(let i = 1; i < this.players.length; i++){
+            const player = this.players[i]
+            if(player.getMoney() > maxAmountOfMoney){
+                maxAmountOfMoney = player.getMoney()
+                this.winner = player
+            }
+        }
     }
 
     public getPlayers(): Player[]{
